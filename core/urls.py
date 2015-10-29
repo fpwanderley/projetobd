21a5db6b9fd69d45e6a15bb5f8aa5176374fe6cd
caiosfonseca <csf2@cin.ufpe.br>
2015-10-29 15:50:06 -0300

    header_nav - Utilizacao do usuario logado na navbar, modificado o menu para ter as opcoes configuracoes, relatorios e logout, alem de funcionarios para o adm e checkin para o usuario comum, faltam links e o usuario realmente ter perfil de adm. ? index - removido script desnecessario, modificado a hora do total e a hora do checkin para comecar sem nada/vindo do banco ? urls - readicionado home

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from .views import home, user_login, user_logout, user_checkin
 
 urlpatterns = [
+    url(r'^$', home, name='home'),
     url(r'^login/', user_login, name='login'),
     url(r'^login/(?P<next>.*)$', user_login, name='login'),
     url(r'^home/$', home, name='home'),
