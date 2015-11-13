6fcbb3a70379e8be02dfde1d6667a354f5351cb2
caiosfonseca <csf2@cin.ufpe.br>
2015-11-12 22:19:34 -0300

    Modificado o CSS para o charts do dia da semana, Adicionado o charts no JS, adicionado dependendias JS da nvd3, modificado html para ter o json_data e o line-chart.

--- core/static/assets/js/report.js
+++ core/static/assets/js/report.js
@@ -4,6 +4,25 @@ $(document).ready(function() {
         startDate: '-30d',
         endDate: '0'
     });
+
+    nv.addGraph(function() {
+        var chart = nv.models.discreteBarChart()
+          .x(function(d) { return d.label })    //Specify the data accessors.
+          .y(function(d) { return d.value })
+          .staggerLabels(true)    //Too many bars and not enough room? Try staggering labels.
+          .tooltips(false)        //Don't show tooltips
+          .showValues(true)       //...instead, show the bar value right on top of each bar.
+          .transitionDuration(350)
+          ;
+
+        d3.select('#nvd3_chart svg')
+          .datum([exampleData()])
+          .call(chart);
+
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+    });
 });
 
 var csrftoken = Cookies.get('csrftoken');
@@ -67,4 +86,45 @@ function reportPost(type, information){
       error: function(data) {
       }
     });
+}
+
+//Each bar represents a single discrete quantity.
+function exampleData() {
+    data = {
+      key: "Dias da semana",
+      values: [
+        { 
+          "color" : "#2CA044",
+          "label" : "Domingo" ,
+          "value" : 6
+        } , 
+        { 
+          "color" : "#D62728",
+          "label" : "Segunda" , 
+          "value" : 4
+        } , 
+        { 
+          "label" : "Terça" , 
+          "value" : 8
+        } , 
+        { 
+          "label" : "Quarta" , 
+          "value" : 6
+        } , 
+        { 
+          "label" : "Quinta" ,
+          "value" : 5
+        } , 
+        { 
+          "label" : "Sexta" , 
+          "value" : 7
+        } , 
+        { 
+          "label" : "Sabado" , 
+          "value" : 6
+        }
+      ]
+    };
+ return data;    
+
 }
