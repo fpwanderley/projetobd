15f910384bf134c68a16a99c20f06085d2452840
caiosfonseca <csf2@cin.ufpe.br>
2015-11-16 20:29:15 -0300

    Adicionado a opcao de adm e modificado o js para o mesmo

--- core/static/assets/js/report.js
+++ core/static/assets/js/report.js
@@ -8,24 +8,48 @@ $(document).ready(function() {
 });
 
 function generate_chart(data){
-    nv.addGraph(function() {
-        var chart = nv.models.discreteBarChart()
-          .x(function(d) { return d.label })    //Specify the data accessors.
-          .y(function(d) { return d.value })
-          .staggerLabels(true)    //Too many bars and not enough room? Try staggering labels.
-          .tooltips(false)        //Don't show tooltips
-          .showValues(true)       //...instead, show the bar value right on top of each bar.
-          .transitionDuration(350)
-          ;
+    if(report_type == "user"){   
+        nv.addGraph(function() {
+            var chart = nv.models.discreteBarChart()
+              .x(function(d) { return d.label })    //Specify the data accessors.
+              .y(function(d) { return d.value })
+              .staggerLabels(true)    //Too many bars and not enough room? Try staggering labels.
+              .tooltips(false)        //Don't show tooltips
+              .showValues(true)       //...instead, show the bar value right on top of each bar.
+              .transitionDuration(350)
+              ;
 
-        d3.select('#nvd3_chart svg')
-          .datum([data])
-          .call(chart);
+            d3.select('#nvd3_chart svg')
+              .datum([data])
+              .call(chart);
 
-        nv.utils.windowResize(chart.update);
+            nv.utils.windowResize(chart.update);
 
-        return chart;
-    });
+            return chart;
+        });
+    }else if (report_type == "adm"){
+        nv.addGraph(function() {
+            var chart = nv.models.multiBarHorizontalChart()
+                .x(function(d) { return d.label })
+                .y(function(d) { return d.value })
+                .margin({top: 30, right: 20, bottom: 50, left: 175})
+                .showValues(true)           //Show bar value next to each bar.
+                .tooltips(true)             //Show tooltips on hover.
+                .transitionDuration(350)
+                .showControls(true);        //Allow user to switch between "Grouped" and "Stacked" mode.
+
+            chart.yAxis
+                .tickFormat(d3.format(',.2f'));
+            // Talvez tenha que por o data em uma lista
+            d3.select('#nvd3_chart svg')
+                .datum([data])
+                .call(chart);
+
+            nv.utils.windowResize(chart.update);
+
+            return chart;
+        });
+    }
 }
 
 var csrftoken = Cookies.get('csrftoken');
